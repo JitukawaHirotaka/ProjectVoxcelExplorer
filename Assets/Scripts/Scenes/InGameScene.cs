dfc34a13ffa7535378dae60699fb4607bcaf3e98
Jitsukawa Hirotaka <jitu8726@gmail.com>
2018-01-31 13:07:41 +0900

    仮シーンの作成
    各シーン用スクリプトを追加

---
+++ Assets/Scripts/Scenes/InGameScene.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// InGameScene class
+/// </summary>
+public class InGameScene : SceneBase<InGameScene>
+{
+	public override ISceneCache SceneCache
+	{
+		get { return null; }
+	}
+
+	protected override InGameScene GetOverrideInstance()
+	{
+		return this;
+	}
+}
