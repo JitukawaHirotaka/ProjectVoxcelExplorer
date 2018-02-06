f196d4290e691e3b177d8dc5c4cb800474f584ea
Jitsukawa Hirotaka <jitu8726@gmail.com>
2018-02-06 11:27:07 +0900

    refactor：タイトルを新しい仕様に適応

---
+++ Assets/Scripts/Scenes/SceneCache/TitleSceneCache.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// タイトルシーンのキャッシュデータ
+/// </summary>
+public class TitleSceneCache : SceneCacheBase, ISceneCache
+{
+	public override SceneRootManager.SceneType RootSceneType
+	{
+		get { return SceneRootManager.SceneType.Title; }
+	}
+}
