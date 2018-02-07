a73248a8f642c86240cf5f0add5b16e74daabb86
Jitsukawa Hirotaka <jitu8726@gmail.com>
2018-02-07 14:30:08 +0900

    ローディングディスプレイの追加

---
+++ Assets/Scripts/Displays/LoadDisplay.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// LoadDisplay class
+/// </summary>
+public class LoadDisplay : DisplayBase
+{
+	/// <summary>
+	/// ディスプレイイベントの定義インターフェイス
+	/// </summary>
+	public override IEvents DisplayEvents
+	{
+		get { return _events; }
+	}
+
+	/// <summary>
+	/// UIオブジェクト呼び出しイベントクラス
+	/// </summary>
+	public EventsBase _events = new EventsBase();
+}
