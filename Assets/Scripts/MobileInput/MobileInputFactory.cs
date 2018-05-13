6e867ed0bcb863d0a350f7a4a7438fe301aa603b
Jitsukawa Hirotaka <jitu8726@gmail.com>
2018-05-14 05:02:55 +0900

    feat : 入力処理生成クラスの実装

---
+++ Assets/Scripts/MobileInput/MobileInputFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace MobileInput
+{   
+    /// <summary>
+    /// 入力クラスを生成する
+    /// </summary>
+    public class Factory
+    {
+        public static ISystemProcess Instantiate()
+        {
+            return new Manager();
+        }
+    }
+}
