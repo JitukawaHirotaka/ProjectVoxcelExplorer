56e938b8eae38b43a0efe3e3161074d976421af8
Jitsukawa Hirotaka <jitu8726@gmail.com>
2018-05-14 04:40:08 +0900

    feat : システムクラス用インターフェイスの作成

---
+++ Assets/Scripts/ISystemProcess.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// SystemProcessクラスで稼働させるシステムクラス用のインターフェイス
+/// </summary>
+public interface ISystemProcess
+{
+    /// <summary>
+    /// 当インターフェイス継承クラスの初期化時に呼び出されるイベント関数
+    /// </summary>
+    void OnStart();
+
+    /// <summary>
+    /// 当インターフェイス継承クラスの毎フレーム呼び出されるイベント関数
+    /// </summary>
+    void OnUpdate();
+
+    /// <summary>
+    /// 当インターフェイス継承クラスの終了時に呼び出されるイベント関数
+    /// </summary>
+    void OnTeam();
+}
