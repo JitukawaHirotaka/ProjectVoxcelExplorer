a8e6e39cb4fcbac999ff046430354ed5a4c7e0d8
Jitsukawa Hirotaka <jitu8726@gmail.com>
2018-05-14 05:03:27 +0900

    feat : システムクラス進行管理クラスの実装

---
+++ Assets/Scripts/SystemProcess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// システムクラスの生成と更新を行うクラス
+/// 仕様に合わせて変更する可能性あり
+/// </summary>
+public class SystemProcess : MonoBehaviour
+{
+    private List<ISystemProcess> processes = null;
+
+	void Start () 
+    {
+        processes = new List<ISystemProcess>();
+        processes.Add(MobileInput.Factory.Instantiate());
+
+        foreach(var value in processes)
+        {
+            value.OnStart();
+        }
+    }
+
+	void Update () 
+    {
+        foreach(var value in processes)
+        {
+            value.OnUpdate();
+        }
+	}
+}
